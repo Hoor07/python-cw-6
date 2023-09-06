fdce290966020c8239bfe557559f816f84bec040
Hoor07 <142871314+Hoor07@users.noreply.github.com>
2023-09-06 20:05:13 +0300

    cw6

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-# write your code here
+person = { 
+         "name": "hoor",
+         "age": "24",
+        "hobbies":["playing piano" , 'shopping' ,'swimming']
+}
+print(person ["name"])
+print(person["age"])
+
+person["age"]=14
+person["country"]=["russia"]
+print(person)
+print(len(person))
+person["hobbies"].append('runing')
+print(person)
+def check_hobbies (person):
+    if len(person["hobbies"]) > 3 :
+        print("WOW YOU ARE AMAZING")   
+    else: 
+         print("add more hobbiess")    
+check_hobbies (person)
